658c23d7d304f943a8b3ca5045c21506db5c7746
miakish <miakish06@mail.ru>
2023-04-06 11:12:47 +0500

    fix error in create Main/RouteBuilder/Bean

--- src/main/java/ru/skb/lab/intellij/plugins/action/project/CreateSmxBaseBeansAction.java
+++ src/main/java/ru/skb/lab/intellij/plugins/action/project/CreateSmxBaseBeansAction.java
@@ -10,6 +10,10 @@ import com.intellij.psi.JavaDirectoryService;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiDirectory;
 import org.jetbrains.annotations.NotNull;
+import ru.skb.lab.intellij.plugins.ui.AppType;
+import ru.skb.lab.intellij.plugins.ui.JavaType;
+import ru.skb.lab.intellij.plugins.ui.ProfileType;
+import ru.skb.lab.intellij.plugins.ui.ProjectType;
 import ru.skb.lab.intellij.plugins.util.Util;
 
 import java.util.ArrayList;
@@ -34,6 +38,17 @@ public class CreateSmxBaseBeansAction extends AnAction {
         Map<String, String> additionalProperties = new HashMap<>();
         additionalProperties.put("GIT_BRANCH", Util.getGitBranch(project));
         additionalProperties.put("COMMENT", "");
+        additionalProperties.put("JAVA_VERSION", "JAVA_11");
+        additionalProperties.put("PROJECT_PACKAGE", "");
+        additionalProperties.put("ACTIVEMQ_ENABLED", String.valueOf(true));
+        additionalProperties.put("SSLCONTEXT_ENABLED", String.valueOf(true));
+        additionalProperties.put("JDBCTEMPLATE_ENABLED", String.valueOf(false));
+        additionalProperties.put("SQLCOMPONENT_ENABLED", String.valueOf(false));
+        additionalProperties.put("CRYPTOPRO_METRICS_ENABLED", String.valueOf(false));
+        additionalProperties.put("DATASOURCE_ENABLED", String.valueOf(false));
+        additionalProperties.put("KAFKACOMPONENT_ENABLED", String.valueOf(false));
+        additionalProperties.put("HTTPCLIENTCONFIGURER_ENABLED", String.valueOf(false));
+
         psiClassList.add(JavaDirectoryService.getInstance().createClass(psiDirectory, "Custom", "SLT_RouteBuilder.java", true, additionalProperties));
         additionalProperties.put("REG_NAME", "operation");
         psiClassList.add(JavaDirectoryService.getInstance().createClass(psiDirectory, "Operation", "SLT_Bean.java", true, additionalProperties));
